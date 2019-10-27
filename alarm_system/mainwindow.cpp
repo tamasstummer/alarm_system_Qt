b22bde73cec3f7f452b291bed1d8e58b5908a593
Stummer Tamas <tamas.stummer@gmail.com>
2019-10-27 19:56:58 +0100

    alarm class created, basic serial communication implemented

--- alarm_system/mainwindow.cpp
+++ alarm_system/mainwindow.cpp
@@ -1,15 +1,29 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QDebug>
+#include <alarm.h>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    Alarm myAlarm;
+    //myAlarm.AvailablePorts();
+
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+
+
 
--- alarm_system/mainwindow.h
+++ alarm_system/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QSerialPort *arduino;
+
 };
 #endif // MAINWINDOW_H
